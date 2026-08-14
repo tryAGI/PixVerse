ac9d4b901c9ec71c1c37497af70da933c6cdd6b7
Konstantin S. <havendv@gmail.com>
2026-08-14 19:42:52 +0400

    feat: Updated OpenAPI spec (#16)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/PixVerse/Generated/PixVerse.PixVerseClient.g.cs
+++ src/libs/PixVerse/Generated/PixVerse.PixVerseClient.g.cs
@@ -34,10 +34,17 @@ namespace PixVerse
 
         /// <inheritdoc/>
         public global::PixVerse.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::PixVerse.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::PixVerse.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
